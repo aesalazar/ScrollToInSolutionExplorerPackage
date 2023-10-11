4e874375c193adfb4399abe1be8ff55315e75ce4
aesalazar <salazare@verizon.net>
2023-10-11 19:00:36 -0400

    semi working code using OleMenuCommand
    The first version uses the community vsix helper library but that did
    not work with non-code type files, e.g. PNGs.  This uses a similar
    technique found in the FindInSolutionExplorer package.
    
    Specifically, it uses the trick of turning on file selection tracking in
    solution explorer temporarily so VS does the work and then giving
    focus to Solution Explorer.  However, in VS2022, there seems to be
    some kind of race condition as it only works abou 80% of the time.

---
+++ ScrollToInSolutionExplorer/FindInSolutionExplorerCommand.cs
@@ -0,0 +1,121 @@
+﻿#nullable enable
+
+using System;
+using System.Linq;
+using Microsoft.VisualStudio;
+using Microsoft.VisualStudio.Shell;
+using System.Composition;
+using System.ComponentModel.Design;
+
+namespace ScrollToInSolutionExplorer
+{
+    internal class FindInSolutionExplorerCommand
+    {
+        private OleMenuCommand _command;
+
+        public FindInSolutionExplorerCommand()
+        {
+            _command = new OleMenuCommand(
+                OnMenuCommandInvoke, 
+                OnMenuCommandChange, 
+                OnMenuCommandBeforeQueryStatus,
+                new CommandID(guidFindInSolutionExplorerCommandSet, cmdidFindInSolutionExplorer));
+
+            var mcs = (IMenuCommandService)ServiceProvider.GetService(typeof(IMenuCommandService));
+            mcs.AddCommand(_command);
+        }
+
+        public static readonly int cmdidFindInSolutionExplorer = 0x0100;
+
+        public const string guidFindInSolutionExplorerCommandSetString = "B3AD9EAD-9439-445D-BECB-6176098247AC";
+        public static readonly Guid guidFindInSolutionExplorerCommandSet = new Guid("{" + guidFindInSolutionExplorerCommandSetString + "}");
+
+        public static EnvDTE80.Window2? FindWindow(EnvDTE80.Windows2 windows, EnvDTE.vsWindowType vsWindowType)
+        {
+            return windows
+                .Cast<EnvDTE80.Window2>()
+                .FirstOrDefault(w => w.Type == vsWindowType);
+        }
+
+        [Import]
+        public SVsServiceProvider ServiceProvider { get; set; }
+
+        public EnvDTE80.DTE2 ApplicationObject => (EnvDTE80.DTE2) ServiceProvider.GetService(typeof(EnvDTE._DTE));
+
+        private void OnMenuCommandInvoke(object sender, EventArgs e)
+        {
+            ThreadHelper.ThrowIfNotOnUIThread();
+            try
+            {
+                var track = ApplicationObject
+                    .get_Properties("Environment", "ProjectsAndSolution")
+                    .Item("TrackFileSelectionInExplorer");
+
+                if (track.Value is bool && !((bool)track.Value))
+                {
+                    track.Value = true;
+                    track.Value = false;
+                }
+
+                // Find the Solution Explorer object
+                var windows = ApplicationObject.Windows as EnvDTE80.Windows2;
+                var solutionExplorer = FindWindow(windows, EnvDTE.vsWindowType.vsWindowTypeSolutionExplorer);
+                if (solutionExplorer != null)
+                    solutionExplorer.Activate();
+            }
+            catch (Exception ex)
+            {
+                if (ErrorHandler.IsCriticalException(ex))
+                    throw;
+            }
+        }
+
+        private void OnMenuCommandChange(object sender, EventArgs e)
+        {
+        }
+
+        private void OnMenuCommandBeforeQueryStatus(object sender, EventArgs e)
+        {
+            ThreadHelper.ThrowIfNotOnUIThread();
+            try
+            {
+                EnvDTE.Document doc = ApplicationObject.ActiveDocument;
+
+                _command.Supported = true;
+
+                bool enabled = false;
+                EnvDTE.ProjectItem projectItem = doc != null ? doc.ProjectItem : null;
+                if (projectItem != null)
+                {
+                    if (projectItem.Document != null)
+                    {
+                        // normal project documents
+                        enabled = true;
+                    }
+                    else if (projectItem.ContainingProject != null)
+                    {
+                        // this applies to files in the "Solution Files" folder
+                        enabled = projectItem.ContainingProject.Object != null;
+                    }
+                }
+
+                _command.Enabled = enabled;
+            }
+            catch (ArgumentException)
+            {
+                // stupid thing throws if the active window is a C# project properties pane
+                _command.Supported = false;
+                _command.Enabled = false;
+            }
+            catch (Exception ex)
+            {
+                if (ErrorHandler.IsCriticalException(ex))
+                    throw;
+
+                _command.Supported = false;
+                _command.Enabled = false;
+            }
+        }
+
+    }
+}
--- ScrollToInSolutionExplorer/ScrollToInSolutionExplorerCommand.cs
+++ ScrollToInSolutionExplorer/ScrollToInSolutionExplorerCommand.cs
@@ -4,7 +4,9 @@ using System.ComponentModel.Design;
 using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
-using Community.VisualStudio.Toolkit;
+using System.Windows.Threading;
+using EnvDTE;
+using Microsoft.VisualStudio;
 using Microsoft.VisualStudio.Shell;
 
 namespace ScrollToInSolutionExplorer
@@ -14,53 +16,15 @@ namespace ScrollToInSolutionExplorer
     /// </summary>
     internal sealed class ScrollToInSolutionExplorerCommand
     {
-        /// <summary>
-        /// Command ID.
-        /// </summary>
-        public const int CommandId = 0x0100;
-
-        /// <summary>
-        /// Command menu group (command set GUID).
-        /// </summary>
-        public static readonly Guid CommandSet = new Guid("d41f8793-ad72-4ed1-af0f-cec3de2b9a61");
-
-        /// <summary>
-        /// VS Package that provides this command, not null.
-        /// </summary>
-        private readonly AsyncPackage package;
-
-        /// <summary>
-        /// Initializes a new instance of the <see cref="ScrollToInSolutionExplorerCommand"/> class.
-        /// Adds our command handlers for menu (commands must exist in the command table file)
-        /// </summary>
-        /// <param name="package">Owner package, not null.</param>
-        /// <param name="commandService">Command service to add command to, not null.</param>
-        private ScrollToInSolutionExplorerCommand(AsyncPackage package, OleMenuCommandService? commandService)
-        {
-            this.package = package ?? throw new ArgumentNullException(nameof(package));
-            commandService = commandService ?? throw new ArgumentNullException(nameof(commandService));
-
-            var menuCommandID = new CommandID(CommandSet, CommandId);
-            var menuItem = new MenuCommand(Execute, menuCommandID);
-            commandService.AddCommand(menuItem);
-        }
+        #region Public Statics
 
         /// <summary>
         /// Gets the instance of the command.
         /// </summary>
-        public static ScrollToInSolutionExplorerCommand? Instance
-        {
-            get;
-            private set;
-        }
+        public static ScrollToInSolutionExplorerCommand? Instance { get; private set; }
 
         /// <summary>
-        /// Gets the service provider from the owner package.
-        /// </summary>
-        private IAsyncServiceProvider ServiceProvider => package;
-
-        /// <summary>
-        /// Initializes the singleton instance of the command.
+        /// Initializes the singleton instance of the command that is registered with the command service.
         /// </summary>
         /// <param name="package">Owner package, not null.</param>
         /// <remarks>
@@ -69,73 +33,175 @@ namespace ScrollToInSolutionExplorer
         public static async Task InitializeAsync(AsyncPackage package)
         {
             await ThreadHelper.JoinableTaskFactory.SwitchToMainThreadAsync(package.DisposalToken);
-
-            var commandService = await package.GetServiceAsync(typeof(IMenuCommandService)) as OleMenuCommandService;
-            Instance = new ScrollToInSolutionExplorerCommand(package, commandService);
+            var applicationObject = (EnvDTE80.DTE2)await package.GetServiceAsync(typeof(EnvDTE._DTE));
+            var commandService = (OleMenuCommandService)await package.GetServiceAsync(typeof(IMenuCommandService));
+            Instance = new ScrollToInSolutionExplorerCommand(commandService, applicationObject);
         }
 
-        private void Execute(object sender, EventArgs e)
-        {
-            _ = Task.Run(() => ExecuteAsync(sender, e));
-        }
+        #endregion
+
+        #region Constructor
 
         /// <summary>
-        /// This function is the callback used to execute the command when the menu item is clicked.
-        /// See the constructor to see how the menu item is associated with this function using
-        /// OleMenuCommandService service and MenuCommand class.
+        /// Initializes a new instance of the <see cref="ScrollToInSolutionExplorerCommand"/> class.
+        /// Adds our command handlers for menu (commands must exist in the command table file)
         /// </summary>
-        /// <param name="sender">Event sender.</param>
-        /// <param name="e">Event args.</param>
-        private async Task ExecuteAsync(object sender, EventArgs e)
+        /// <param name="commandService">Command service to add command to, not null.</param>
+        /// <param name="visualStudioInstance">Reference to the Visual Studio instance.</param>
+        private ScrollToInSolutionExplorerCommand(
+            OleMenuCommandService commandService,
+            EnvDTE80.DTE2 visualStudioInstance)
+        {
+            _visualStudioInstance = visualStudioInstance;
+            _command = new OleMenuCommand(
+                OnMenuCommandInvoke,
+                OnMenuCommandChange,
+                OnMenuCommandBeforeQueryStatus,
+                new CommandID(CommandSet, CommandId)
+            );
+
+            commandService.AddCommand(_command);
+        }
+
+        #endregion
+
+        #region Fields
+
+        /// <summary>
+        /// Command ID.
+        /// </summary>
+        private const int CommandId = 0x0100;
+
+        /// <summary>
+        /// Command menu group (command set GUID).
+        /// </summary>
+        private static readonly Guid CommandSet = new Guid("d41f8793-ad72-4ed1-af0f-cec3de2b9a61");
+
+        /// <summary>
+        /// Regisered Command
+        /// </summary>
+        private OleMenuCommand _command;
+
+        /// <summary>
+        /// Reference to Visual Studio.
+        /// </summary>
+        private EnvDTE80.DTE2 _visualStudioInstance;
+
+        #endregion
+
+        #region Methods
+
+        private static EnvDTE80.Window2? FindWindow(EnvDTE80.Windows2 windows, EnvDTE.vsWindowType vsWindowType)
+        {
+            return windows
+                .Cast<EnvDTE80.Window2>()
+                .FirstOrDefault(w => w.Type == vsWindowType);
+        }
+
+        #endregion
+
+        #region Command Handlers
+
+        private void OnMenuCommandInvoke(object sender, EventArgs e)
+        {
+            _ = Task.Run(() => OnMenuCommandInvokeAsync(sender, e));
+        }
+
+        private async Task OnMenuCommandInvokeAsync(object sender, EventArgs e)
         {
             await ThreadHelper.JoinableTaskFactory.SwitchToMainThreadAsync();
-
-            //Look for a match by path
-            var documentView = await VS.Documents.GetActiveDocumentViewAsync();
-            var path = documentView?.FilePath;
-            Debug.WriteLine($"CURRENT DOCUMENT:  {path}");
-            if (string.IsNullOrEmpty(path))
-                return;
-
-            //Look for the root node
-            var solutionExplorer = await VS.Windows.GetSolutionExplorerWindowAsync();
-            if (solutionExplorer == null)
-                return;
-
-            var selections = await solutionExplorer.GetSelectionAsync();
-            if (!selections.Any())
-                return;
-
-            var rootNode = selections.First();
-            while (rootNode.Parent != null)
+            try
             {
-                rootNode = rootNode.Parent;
-            }
+                //Use the VS track file selection to actually select
+                var track = _visualStudioInstance
+                    .get_Properties("Environment", "ProjectsAndSolution")
+                    .Item("TrackFileSelectionInExplorer");
 
-            //Find a match
-            var match = default(SolutionItem);
-            SolutionExplorerHelpers.TraverseChildren(
-                rootNode
-                , solutionItem =>
+                //Activate if currently off
+                var isTrackingSelection = track.Value is true;
+                if (!isTrackingSelection)
+                    track.Value = true;
+                          
+                Debug.WriteLine($"INFO: Track Value 1= {track.Value}");
+                
+                try
                 {
-                    Debug.WriteLine($"AVAILABLE DOCUMENT: {solutionItem.Type}: {solutionItem.Name}");
-                    if (solutionItem.Type == SolutionItemType.Unknown
-                        || solutionItem.Type == SolutionItemType.VirtualFolder
-                        || solutionItem.FullPath != path)
-                        return true;
+                    // Find the Solution Explorer object
+                    var windows = (EnvDTE80.Windows2)_visualStudioInstance.Windows;
+                    var solutionExplorer = FindWindow(windows, vsWindowType.vsWindowTypeSolutionExplorer);
 
-                    match = solutionItem;
-                    return false;
-                });
-
-            if (match == default)
-            {
-                Debug.WriteLine($"Could not find file in Solution Explorer: {path}");
-                return;
+                    Debug.WriteLine($"INFO: SE = {solutionExplorer}");
+                    Debug.WriteLine($"INFO: SE Document = {solutionExplorer?.Document?.Name}");
+                    if (solutionExplorer != null)
+                        solutionExplorer.Activate();
+                }
+                finally
+                {
+                    //Restore the prior state if neccessary
+                    if (!isTrackingSelection)
+                        await ThreadHelper.JoinableTaskFactory.RunAsync(async () =>
+                        {
+                            await ThreadHelper.JoinableTaskFactory.SwitchToMainThreadAsync();
+                            track.Value = false;
+                            Debug.WriteLine($"INFO: Track Value 2= {track.Value}");
+                        });
+                }
             }
-
-            solutionExplorer.SetSelection(match);
-            Debug.WriteLine($"Selected file in Solution Explorer: {path}");
+            catch (Exception ex)
+            {
+                if (ErrorHandler.IsCriticalException(ex))
+                    throw;
+            }
         }
+
+        private void OnMenuCommandChange(object sender, EventArgs e)
+        {
+        }
+
+        private void OnMenuCommandBeforeQueryStatus(object sender, EventArgs e)
+        {
+            ThreadHelper.ThrowIfNotOnUIThread();
+            try
+            {
+                var doc = _visualStudioInstance.ActiveDocument;
+                _command.Supported = true;
+
+                var isCommandEnabled = false;
+                var projectItem = doc?.ProjectItem;
+                if (projectItem != null)
+                {
+                    Debug.WriteLine($"INFO: Item = {projectItem.Name} - {projectItem.Kind}");
+
+                    if (projectItem.Document != null)
+                    {
+                        // normal project documents
+                        isCommandEnabled = true;
+                    }
+                    else if (projectItem.ContainingProject != null)
+                    {
+                        // this applies to files in the "Solution Files" folder
+                        isCommandEnabled = projectItem.ContainingProject.Object != null;
+                    }
+                }
+
+                _command.Enabled = isCommandEnabled;
+            }
+            catch (ArgumentException)
+            {
+                // stupid thing throws if the active window is a C# project properties pane
+                _command.Supported = false;
+                _command.Enabled = false;
+            }
+            catch (Exception ex)
+            {
+                if (ErrorHandler.IsCriticalException(ex))
+                    throw;
+
+                _command.Supported = false;
+                _command.Enabled = false;
+            }
+        }
+
+        #endregion
     }
 }
