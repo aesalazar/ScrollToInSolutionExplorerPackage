fa8bfe01b383dbe7f8710dd27800a9226b54f780
aesalazar <salazare@verizon.net>
2023-10-14 16:50:21 -0400

    working version but using Task.Delay
    This works reliably by using await Task.Delay(500).  But this is not an
    ideal solution and must look for a less arbitrary solution.

--- ScrollToInSolutionExplorer/ScrollToInSolutionExplorerCommand.cs
+++ ScrollToInSolutionExplorer/ScrollToInSolutionExplorerCommand.cs
@@ -1,13 +1,17 @@
 ﻿#nullable enable
 using System;
+using System.Collections;
 using System.ComponentModel.Design;
 using System.Diagnostics;
 using System.Linq;
+using System.Reflection;
+using System.Threading;
 using System.Threading.Tasks;
-using System.Windows.Threading;
+using Community.VisualStudio.Toolkit;
 using EnvDTE;
 using Microsoft.VisualStudio;
 using Microsoft.VisualStudio.Shell;
+using Microsoft.VisualStudio.Shell.Interop;
 
 namespace ScrollToInSolutionExplorer
 {
@@ -33,9 +37,9 @@ namespace ScrollToInSolutionExplorer
         public static async Task InitializeAsync(AsyncPackage package)
         {
             await ThreadHelper.JoinableTaskFactory.SwitchToMainThreadAsync(package.DisposalToken);
-            var applicationObject = (EnvDTE80.DTE2)await package.GetServiceAsync(typeof(EnvDTE._DTE));
+            var applicationObject = (EnvDTE80.DTE2)await package.GetServiceAsync(typeof(_DTE));
             var commandService = (OleMenuCommandService)await package.GetServiceAsync(typeof(IMenuCommandService));
-            Instance = new ScrollToInSolutionExplorerCommand(commandService, applicationObject);
+            Instance = new ScrollToInSolutionExplorerCommand(commandService, applicationObject, package);
         }
 
         #endregion
@@ -50,9 +54,11 @@ namespace ScrollToInSolutionExplorer
         /// <param name="visualStudioInstance">Reference to the Visual Studio instance.</param>
         private ScrollToInSolutionExplorerCommand(
             OleMenuCommandService commandService,
-            EnvDTE80.DTE2 visualStudioInstance)
+            EnvDTE80.DTE2 visualStudioInstance,
+            AsyncPackage package)
         {
             _visualStudioInstance = visualStudioInstance;
+            _package = package;
             _command = new OleMenuCommand(
                 OnMenuCommandInvoke,
                 OnMenuCommandChange,
@@ -87,6 +93,8 @@ namespace ScrollToInSolutionExplorer
         /// </summary>
         private EnvDTE80.DTE2 _visualStudioInstance;
 
+        private readonly AsyncPackage _package;
+
         #endregion
 
         #region Methods
@@ -107,45 +115,122 @@ namespace ScrollToInSolutionExplorer
             _ = Task.Run(() => OnMenuCommandInvokeAsync(sender, e));
         }
 
+
+        private async Task SelectCurrentDocumentAsync()
+        {
+            var docView = await VS.Documents.GetActiveDocumentViewAsync();
+
+            if (docView?.FilePath != null)
+            {
+                var item = await PhysicalFile.FromFileAsync(docView.FilePath);
+
+                // Find the Solution Explorer object
+                var windows = (EnvDTE80.Windows2)_visualStudioInstance.Windows;
+                var solutionExplorer = FindWindow(windows, vsWindowType.vsWindowTypeSolutionExplorer);
+                if (solutionExplorer != null)
+                {
+                    Debug.WriteLine($"INFO: SE Document = {solutionExplorer.Document?.Name}");
+                    solutionExplorer.Set()
+                }
+
+
+            }
+        }
+
+        private async Task<string?> SelectedItemFilePathAsync()
+        {
+            await ThreadHelper.JoinableTaskFactory.SwitchToMainThreadAsync();
+            var uiHeirarcy = _visualStudioInstance.ToolWindows.SolutionExplorer;
+
+            if (uiHeirarcy.SelectedItems is IEnumerable selectedItems)
+            {
+                foreach (UIHierarchyItem selItem in selectedItems)
+                {
+                    if (selItem.Object is ProjectItem projectItem)
+                    {
+                        Debug.WriteLine($"INFO: prjItem = {projectItem.Name}");
+                        var path = projectItem.Properties.Item("FullPath").Value.ToString(); ;
+                        var item = await PhysicalFile.FromFileAsync(path);
+                        Debug.WriteLine($"INFO: item = {item?.FullPath}");
+                    }
+                }
+
+                foreach (UIHierarchyItem selItem in selectedItems)
+                {
+                    if (selItem.Object is ProjectItem prjItem)
+                        return prjItem.Properties.Item("FullPath").Value.ToString();
+                }
+            }
+
+            return null;
+        }
+
+        private async Task<string?> GetSolutionFilterFilePathAsync()
+        {
+            Community.VisualStudio.Toolkit.Solution? solution;
+            solution = await VS.Solutions.GetCurrentSolutionAsync();
+
+            if (solution is not null)
+            {
+                solution.GetItemInfo(out IVsHierarchy hierarchy, out _, out _);
+                if (hierarchy.GetType().GetProperty("SolutionFilterFilePath") is PropertyInfo filePath)
+                {
+                    return filePath.GetValue(hierarchy) as string;
+                }
+            }
+
+            return null;
+        }
+
         private async Task OnMenuCommandInvokeAsync(object sender, EventArgs e)
         {
             await ThreadHelper.JoinableTaskFactory.SwitchToMainThreadAsync();
             try
             {
+                Debug.WriteLine($"INFO: SelectedItemFilePathAsync = {await SelectedItemFilePathAsync()}");
+
                 //Use the VS track file selection to actually select
-                var track = _visualStudioInstance
-                    .get_Properties("Environment", "ProjectsAndSolution")
+                var trackFileProperty = _visualStudioInstance
+                    .Properties["Environment", "ProjectsAndSolution"]
                     .Item("TrackFileSelectionInExplorer");
 
                 //Activate if currently off
-                var isTrackingSelection = track.Value is true;
+                var isTrackingSelection = trackFileProperty.Value is true;
                 if (!isTrackingSelection)
-                    track.Value = true;
-                          
-                Debug.WriteLine($"INFO: Track Value 1= {track.Value}");
-                
-                try
                 {
-                    // Find the Solution Explorer object
-                    var windows = (EnvDTE80.Windows2)_visualStudioInstance.Windows;
-                    var solutionExplorer = FindWindow(windows, vsWindowType.vsWindowTypeSolutionExplorer);
+                    trackFileProperty.Value = true;
+                    await Task.Delay(500).ConfigureAwait(true); // YIKES!!!!
+                    trackFileProperty.DTE.MainWindow.Activate();
+                    Debug.WriteLine($"INFO: Track Value 1 = {trackFileProperty.Value}");
+                }
 
-                    Debug.WriteLine($"INFO: SE = {solutionExplorer}");
-                    Debug.WriteLine($"INFO: SE Document = {solutionExplorer?.Document?.Name}");
-                    if (solutionExplorer != null)
-                        solutionExplorer.Activate();
-                }
-                finally
+                await Task.Factory.StartNew(async () =>
                 {
-                    //Restore the prior state if neccessary
-                    if (!isTrackingSelection)
-                        await ThreadHelper.JoinableTaskFactory.RunAsync(async () =>
+                    await ThreadHelper.JoinableTaskFactory.SwitchToMainThreadAsync();
+
+                    try
+                    {
+                        // Find the Solution Explorer object
+                        var windows = (EnvDTE80.Windows2)_visualStudioInstance.Windows;
+                        var solutionExplorer = FindWindow(windows, vsWindowType.vsWindowTypeSolutionExplorer);
+                        if (solutionExplorer != null)
                         {
-                            await ThreadHelper.JoinableTaskFactory.SwitchToMainThreadAsync();
-                            track.Value = false;
-                            Debug.WriteLine($"INFO: Track Value 2= {track.Value}");
-                        });
-                }
+                            Debug.WriteLine($"INFO: SE Document = {solutionExplorer.Document?.Name}");
+                            solutionExplorer.Activate();
+                        }
+                    }
+                    finally
+                    {
+                        //Restore the prior state if neccessary
+                        if (!isTrackingSelection)
+                            await ThreadHelper.JoinableTaskFactory.RunAsync(async () =>
+                            {
+                                await ThreadHelper.JoinableTaskFactory.SwitchToMainThreadAsync();
+                                trackFileProperty.Value = false;
+                                Debug.WriteLine($"INFO: Track Value 2 = {trackFileProperty.Value}");
+                            });
+                    }
+                }, CancellationToken.None, TaskCreationOptions.RunContinuationsAsynchronously, TaskScheduler.Default);
             }
             catch (Exception ex)
             {
